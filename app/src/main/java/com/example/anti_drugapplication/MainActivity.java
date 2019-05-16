97137bf5c9b383db85d2c7270ef902e7ca5de6a2
raakin <lioneir325@gmail.com>
2019-05-16 11:54:32 -0400

    Added more text in Psychological Impact string

--- app/src/main/java/com/example/anti_drugapplication/MainActivity.java
+++ app/src/main/java/com/example/anti_drugapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.anti_drugapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -65,7 +66,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -84,6 +85,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_disclaimer) {
             Intent intent = new Intent(MainActivity.this, Disclaimer.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_Helpful) {
+            Intent intent = new Intent(MainActivity.this, Helpful_Numbers.class);
             startActivity(intent);
         }
 
